71de4b08c1194540bdb284a3e2ff3a23263498f3
Jazzberries <anime.flick.chick@gmail.com>
2022-05-07 16:37:09 +1000

    Added post function, added uuid npm

--- develop/server.js
+++ develop/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const db = require("./db/db.json")
+const uuid = require("uuid");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,9 +26,37 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received to add a note.`)
 
-    let note;
+    const { title, text } = req.body;
 
+    //if the post request contains title and text then create a new note
+    if (title && text) {
 
+        let newNote = {
+            id: uuid.v4(),
+            title: req.body.title,
+            text: req.body.text
+        }
+
+        fs.readFile("./db/db.json", "utf-8", (err, noteContent) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const notes = json.parse(noteContent);
+                notes.push(newNote);
+                console.log(noteContent);
+
+                fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (err) => {
+                    err ? console.error(err) : console.log(`New note ${newNote.title} added!`)
+                })
+            }
+        })
+    }
+
+});
+
+app.get("*", (req, res) => {
+    console.log(`${req.method} request received.`);
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () => {
