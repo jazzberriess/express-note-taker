8876ee5043334d0f51eaf74125641b9d904dcb4f
Jazzberries <anime.flick.chick@gmail.com>
2022-05-08 15:02:47 +1000

    Cleaned up redundant code

--- server.js
+++ server.js
@@ -10,71 +10,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//return the notes.html file
+//route handler for the the notes.html file
 app.get("/notes", (req, res) => {
     console.log(`${req.method} request received.`);
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-//request notes data from the db.json file
+//route handler to retrieve notes data from the db.json file
 app.get("/api/notes", (req, res) => {
     console.log(`${req.method} request received to retrieve notes.`)
-    // for (let i = 0; i < notesDb.length; i++) {
-    //     console.log(notesDb[i].id);
-    // }
-
     res.json(notesDb);
 
 });
 
-// app.get("/api/notes/:id", (req, res) => {
-//     const getId = req.params.id
-//     const notesData = notesDb;
-//     console.log(notesData);
-
-//     if (getId) {
-//         for (let i = 0; i < notesData.length; i++) {
-//             if (getId.includes(notesData[i].id)) {
-//                 res.json(notesData);
-//             } else {
-//                 console.log(getId);
-//                 console.log(notesData[12].id);
-//                 return res.json({ Message: `Unable to find note with an ID of ${req.params.id}` })
-//             }
-//         };
-//     }
-// });
-
-// if (getId) {
-//     notesData.forEach(id => {
-//         if (getId === req.params.id) {
-//             console.log(notesData.id + "line32");
-//             res.json(notesData.id);
-//         } else {
-//             res.json({ Message: `Unable to find a note with ID: ${req.params}` });
-//         }
-//     });
-// }
-
-// });
-
-// app.get("/api/notes/:id", (req, res) => {
-//     console.log(`${req.method} request received to retrieve note.`)
-
-
-
-//     const retrieveNote = JSON.parse(db).some(item => item.id === parseInt(req.params.id));
-//     if (retrieveNote) {
-
-//         res.json(db.filter(item => item.id === parseInt(req.params.id)));
-//     } else {
-//         res.json(({ Message: "No note here!" }));
-//         console.log(retrieveNote);
-//     }
-
-// });
-
-//post request to save a new note to the request body and add to the db.json file
+//route handler for the POST request to save a new note to the request body and add to the db.json file
 app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received to add a note.`)
 
@@ -90,14 +39,18 @@ app.post("/api/notes", (req, res) => {
         };
         console.log(newNote);
 
+        //read the db.json file
         fs.readFile("./db/db.json", "utf-8", (err, noteContent) => {
             if (err) {
                 console.error(err);
             } else {
+
+                //push newNote data
                 const notes = JSON.parse(noteContent);
                 notes.push(newNote);
                 console.log(noteContent);
 
+                //write to the db.json file
                 fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (err) => {
                     err ? console.error(err) : console.log(`New note ${newNote.title} added!`)
                 })
@@ -120,18 +73,23 @@ app.post("/api/notes", (req, res) => {
 //delete note
 app.delete("/api/notes/:id", (req, res) => {
     console.log(`${req.method} request received to delete notes.`);
-    // const data = JSON.parse(notesDb);
+
     const noteId = req.params.id;
 
-    console.log(noteId);
+    // console.log(noteId);
 
+    //read the db.json file
     fs.readFile("db/db.json", (err, notesContent) => {
         if (err) {
             console.error(err);
         } else {
             const noteList = JSON.parse(notesContent);
+
+            //filter out the noteId and create a new array of data with the noteId filtered out
             const updatedNoteList = noteList.filter(note => note.id !== noteId);
             console.log(updatedNoteList);
+
+            //write the updatedNoteList to the db.json file
             fs.writeFile("db/db.json", JSON.stringify(updatedNoteList, null, 4), (err) => {
                 err ? console.error(err) : res.json(`Removed Note ID ${noteId}`);
             });
@@ -139,77 +97,6 @@ app.delete("/api/notes/:id", (req, res) => {
         }
     })
 })
-
-// console.log(notesDb);
-
-//     fs.readFile("./db/db.json", "utf-8", (err, noteContent) => {
-//         if (err) {
-//             console.error(err);
-//         } else {
-//             let notes = JSON.parse(noteContent);
-//             console.log(notes.id);
-
-//         }
-//             )
-// }
-//     })
-
-//     for (let i = 0; i < notesDb.length; i++) {
-//         console.log(notesDb[i].id)
-//         const notes = JSON.parse(notesDb);
-
-//         if (noteId === notesDb[i].id) {
-
-//             const updatedNotes = notes.filter(note = notes.id !== noteId);
-
-//             console.log(updatedNotes);
-
-//             fs.writeFile("./db/db.json", JSON.stringify(updatedNotes, null, 4), (err) => {
-//                 err ? console.error(err) : console.log(`Note ${updatedNotes.id} removed!`);
-
-//                 res.json({ Message: `Note ${id} removed!` })
-//             })
-//         }
-//     }
-// });
-
-
-// notesDb.splice(noteIndex);
-
-//         console.log(noteIndex);
-
-//         fs.writeFile("./db/db.json", JSON.stringify(notesDb, null, 4), (err) => {
-//             err ? console.error(err) : console.log(`Note ${id} removed!`)
-//             // })
-//         })
-//     }
-//     else {
-// res.send({ Message: "Oops! Unable to delete that ID!" })
-//         console.log(id, "line 140")
-//         console.log("Boo");
-//     }
-// }
-// }
-
-// notes.splice()
-
-// data.forEach(note => {
-//     if (note.id === req.params.id) {
-//         res.json({ Message: `Note ${note.id} removed!` })
-
-
-//         const noteId = notes.some(note => note.id === req.params.id);
-//         note.splice(noteId);
-//         console.log(noteContent);
-
-//         fs.writeFile("./db/db.json", JSON.stringify(noteContent, null, 4), (err) => {
-//             err ? console.error(err) : console.log(`Note ${noteId.title} removed!`)
-//         })
-//     }
-// })
-// })
-
-// });
 
 //wildcard route to send users to the index.html page
 app.get("*", (req, res) => {
